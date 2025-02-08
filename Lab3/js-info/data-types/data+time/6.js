d6848d7ba7be059e3b786e49482d4cf3355b57d2
karina-kbtu <157091081+karina-kbtu@users.noreply.github.com>
2025-02-08 13:23:11 +0500

    lab3

---
+++ Lab3/js-info/data-types/data+time/6.js
@@ -0,0 +1,11 @@
+function getSecondsToday() {
+    let now = new Date();
+  
+   
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  
+    let diff = now - today; // ms difference
+    return Math.round(diff / 1000); // make seconds
+  }
+  
+  alert( getSecondsToday() );
