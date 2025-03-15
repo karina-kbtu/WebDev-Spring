a2c64eca8ee35839e2d9b51cc73409e56061867a
karina-kbtu <157091081+karina-kbtu@users.noreply.github.com>
2025-03-15 16:31:59 +0500

    lab7

---
+++ Lab7/informatics/yslovnyi-operator/C.py
@@ -0,0 +1,6 @@
+correct_answer = int(input())
+student_answer = int(input())
+if correct_answer == student_answer or correct_answer != 1 and student_answer != 1:
+    print("YES")
+else:
+    print("NO")
